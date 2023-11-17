c49b563dd6979bf48f70adfc4b8bff603db7c03b
EdivaldoPereira <edivaldo.pereira@gmail.com>
2023-11-17 14:38:02 -0300

    finalização do programa

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-afsadf;
+let nomeDoHeroi = "Anônimo"
+let nivelDoHeroi = 4800;
+
+if (nivelDoHeroi <= 1000){
+    console.log("O nome do heroi é " + nomeDoHeroi + " e está na qualidade de ferro")
+} else if (nivelDoHeroi >= 1001 && nivelDoHeroi <= 2000){
+    console.log("O nome do heroi é " + nomeDoHeroi + " e está na qualidade de bronze")
+} else if (nivelDoHeroi >= 2001 && nivelDoHeroi <= 5000){
+    console.log("O nome do heroi é " + nomeDoHeroi + " e está na qualidade de prata")
+} else if (nivelDoHeroi >= 6001 && nivelDoHeroi <= 7000){
+    console.log("O nome do heroi é " + nomeDoHeroi + " e está na qualidade de ouro")
+} else if (nivelDoHeroi >= 7001 && nivelDoHeroi <= 8000){
+    console.log("O nome do heroi é " + nomeDoHeroi + " e está na qualidade de platina")
+} else if (nivelDoHeroi >= 8001 &&nivelDoHeroi <= 9000){
+    console.log("O nome do heroi é " + nomeDoHeroi + " e está na qualidade de ascendente")
+} else if (nivelDoHeroi >= 9001 && nivelDoHeroi <= 10000){
+    console.log("O nome do heroi é " + nomeDoHeroi + " e está na qualidade de imortal")
+} else if (nivelDoHeroi >= 10001){
+    console.log("O nome do heroi é " + nomeDoHeroi + " e está na qualidade de radiante")
+}
+
+
+
